2cc6b6eacbb7af092285c5967f4acc9044060fe3
ArtemF <blademasterii@rambler.ru>
2021-06-14 15:16:58 +0500

    Решение WinFormsControlLab

---
+++ WinFormsControlLab/LabControls/NumberBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabControls
+{
+    public partial class NumberBox : Form
+    {
+        public NumberBox()
+        {
+            InitializeComponent();
+        }
+        public NumberBox(IContainer container)
+        {
+            container.Add(this);
+            InitializeComponent();
+        }
+    }
+}
