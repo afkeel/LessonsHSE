c48c0fd1ecdd1545a710d3859ebc2c1d9902f624
ArtemF <blademasterii@rambler.ru>
2021-06-25 14:48:54 +0500

    Проект WhoWantsToBeAMillionaire.

---
+++ WhoWantsToBeAMillionaire/Main/MainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Main
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "1";
+            this.button1.Text = "btnAnswerA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Tag = "2";
+            this.button2.Text = "btnAnswerB";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(132, 318);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Tag = "3";
+            this.button3.Text = "btnAnswerC";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(338, 318);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Tag = "4";
+            this.button4.Text = "btnAnswerD";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "50/50";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(132, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 153);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(129, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 108);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "lblQuestion";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "3 000 000",
+            "1 500 000",
+            "800 000",
+            "400 000",
+            "200 000 ",
+            "100 000",
+            "50 000",
+            "25 000",
+            "15 000",
+            "10 000",
+            "5 000",
+            "3 000",
+            "2 000",
+            "1 000",
+            "500 "});
+            this.listBox1.Location = new System.Drawing.Point(431, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 290);
+            this.listBox1.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 370);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "WhoWantsToBeAMillionaire";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WhoWantsToBeAMillionaire/Main/MainForm.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class MainForm : Form
+    {
+        List<Question> questions = new List<Question>();
+        private Random rnd = new Random();
+        int level = 0;
+        Question currentQuestion;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            DBConnect();
+            //ReadFile();
+            startGame();
+        }
+        private void DBConnect()
+        {
+            SQLiteConnection cn = new SQLiteConnection(@"Data Source=C:\Users\User\Desktop\Lab8\SQLiteDatabaseBrowserPortable\wwtbam.db");
+            cn.Open();
+            var cmd = new SQLiteCommand("select * from Вопросы", cn);
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                string[] line = { dr["Вопрос"].ToString(), dr["Ответ1"].ToString(),
+                    dr["Ответ2"].ToString(),dr["Ответ3"].ToString(),dr["Ответ4"].ToString(),
+                    dr["ПОтвет"].ToString(), dr["Сложность"].ToString() };
+                questions.Add(new Question(line));
+            }
+            dr.Close();
+            cn.Close();
+        }
+        private void ReadFile()
+        {
+            string path = @"Вопросы.txt";
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    questions.Add(new Question(line.Split('\t')));
+                }
+            }
+        }
+        private void ShowQuestion(Question q)
+        {
+            label1.Text = q.Text;
+            button1.Text = q.Answers[0];
+            button2.Text = q.Answers[1];
+            button3.Text = q.Answers[2];
+            button4.Text = q.Answers[3];
+        }
+        private Question GetQuestion(int level)
+        {
+            var questionsWithLevel = questions.Where(q => q.Level == level).ToList();
+            return questionsWithLevel[rnd.Next(questionsWithLevel.Count)];
+        }
+        private void NextStep()
+        {
+            Button[] btns = new Button[] { button1, button2,
+                button3, button4 };
+
+            foreach (Button btn in btns)
+                btn.Enabled = true;
+
+            level++;
+            currentQuestion = GetQuestion(level);
+            ShowQuestion(currentQuestion);
+            listBox1.SelectedIndex = listBox1.Items.Count - level;
+        }
+        private void startGame()
+        {
+            level = 0;
+            NextStep();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (currentQuestion.RightAnswer == int.Parse(button.Tag.ToString()))
+                NextStep();
+            else
+            {
+                MessageBox.Show("Неверный ответ!");
+                startGame();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (currentQuestion.RightAnswer == int.Parse(button.Tag.ToString()))
+                NextStep();
+            else
+            {
+                MessageBox.Show("Неверный ответ!");
+                startGame();
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (currentQuestion.RightAnswer == int.Parse(button.Tag.ToString()))
+                NextStep();
+            else
+            {
+                MessageBox.Show("Неверный ответ!");
+                startGame();
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (currentQuestion.RightAnswer == int.Parse(button.Tag.ToString()))
+                NextStep();
+            else
+            {
+                MessageBox.Show("Неверный ответ!");
+                startGame();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button[] btns = new Button[] { button1, button2,
+                button3, button4 };
+
+            int count = 0;
+            while (count < 2)
+            {
+                int n = rnd.Next(4);
+                int answer = int.Parse(btns[n].Tag.ToString());
+
+                if (answer != currentQuestion.RightAnswer && btns[n].Enabled)
+                {
+                    btns[n].Enabled = false;
+                    count++;
+                }
+            }
+        }
+    }
+}
