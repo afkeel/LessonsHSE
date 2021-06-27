01dbf595aa24eeae4ba51f17703180d147471c23
ArtemF <blademasterii@rambler.ru>
2021-06-27 11:03:34 +0500

    Lesson9.

---
+++ Lesson9/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task t = new Task(() => Console.WriteLine("Hello"));
+            //t.Start();
+            //Task.Run(() => Console.WriteLine("Hello"));
+            //Parallel.Invoke(
+            //    new Action[] {()=>Console.WriteLine("!!!!!!"),
+            //    ()=>Console.WriteLine("&&&&&&")}
+            //    );
+            ////Parallel.For(1, 10, (i) => Console.WriteLine(i));
+            //Parallel.ForEach(new int[] { 1,2,3,4,5,6,7,8,9}, (i) => Console.WriteLine(i));
+            //Test();
+            //DoWorkS();
+            DoWork();
+        }
+        static async void Test()
+        {
+            var res = await DoWork();
+            Console.WriteLine(res);
+        }
+        static string DoWorkS()
+        {
+            Thread.Sleep(5000);
+            return "Done";
+        }
+        static Task<string> DoWork()
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                return "Done";
+            });
+            
+        }
+    }
+}
---
+++ Lesson9/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Thread.CurrentThread.Name = "Main";
+            //progressBar1.Maximum = 1000000;
+            Thread t1 = new Thread(LongOperation);
+            t1.Name = "LongOperation";
+            t1.Priority = ThreadPriority.AboveNormal;
+            t1.Start(Color.Red);
+
+            Thread t2 = new Thread(LongOperation);
+            t2.Name = "LongOperation";
+            t2.Priority = ThreadPriority.BelowNormal;
+            t2.Start(Color.Green);
+
+            Thread t3 = new Thread(LongOperation);
+            t3.Name = "LongOperation";
+            t3.Priority = ThreadPriority.Normal;
+            t3.Start(Color.Blue);
+        }
+        //private void LongOperation()
+        //{
+        //    bool b = true;            
+        //    for (int i = 0; i < 1000000; i++)
+        //    {
+        //        for (int j = 0; j < 10000; j++)
+        //            b = !b;
+        //        Invoke((Action)delegate() { progressBar1.Value += 1; });                
+        //    }
+        //    MessageBox.Show("Done");
+        //}
+        Random rnd = new Random();
+        bool stop = false;
+        object obj = new object();
+        private void LongOperation(object color)
+        {
+            stop = false;
+            Color c = (Color)color;
+            var g = CreateGraphics();
+            while (!stop)
+            {
+                lock(obj)
+                {
+                    g.FillEllipse(new SolidBrush(c), rnd.Next(ClientSize.Width),
+                    rnd.Next(ClientSize.Height), 10, 10);
+                }              
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+    }
+}
